671df282c23e6b4f8ebafa708911729de2c32788
ryotaro-rks <ryotaro.seya@rakus.co.jp>
2021-05-24 11:29:33 +0900

    従業員の詳細ページを表示する機能の作成
    create html and controller for detail of employee page.
    
    fix bug(request param id for detail, img in detail). fixed bugs: parameter is text, img src is null.

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -3,8 +3,11 @@ package jp.co.sample.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -19,6 +22,11 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 
+	@ModelAttribute
+	private UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+
 	/**
 	 * 従業員情報一覧表示
 	 * 
@@ -29,5 +37,12 @@ public class EmployeeController {
 	public String showList(Model model) {
 		model.addAttribute("employeeList", employeeService.showList());
 		return "employee/list";
+	}
+
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+		return "employee/detail";
 	}
 }
